2883b6194309a349da9b755463ecc71e7494e662
Remarci225 <71138785+Remarci225@users.noreply.github.com>
2021-05-17 20:25:22 +0200

    Kezdő C++ fájl

---
+++ C++/PrimeX.cpp
@@ -0,0 +1,12 @@
+// ======
+// PrimeX
+// ======
+// Egy program a minél gyorsabb prímszámkeresésre
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "======\nPrimeX\n======\n" << std::endl;
+    
+}
